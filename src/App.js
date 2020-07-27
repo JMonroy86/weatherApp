61f8ececd4a03ed03be05ea5533541727d09c83e
Jonathan Monroy <jonathan.monroy@bracket.cl>
2020-07-26 22:54:12 -0400

    adding css to components, changing folders order new weather constant file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import WeatherLocation from './components/weatherLocation';
+import WeatherLocation from './components/WeatherLocation';
 
 
 function App() {
--- src/components/Location.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Location = ({ city } ) => {
-  return(
-    <h1>{city}</h1>
-  )
-}
-export default Location;
